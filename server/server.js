17746dccfb5ff681212725c23e3a1d53ce097ac0
PranavP2k1 <145428124+PranavP2k1@users.noreply.github.com>
2023-09-26 16:18:44 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,21 +1,28 @@
 require("dotenv").config()
-//hello
+
 const express = require("express")
 const app = express()
 const cors = require("cors")
-app.use(express.json())
+app.use(express.json()) //To read all the json data send to us from the client side.
 app.use(
   cors({
     origin: "http://localhost:5500",
   })
 )
 
-const stripe = require("stripe")(process.env.STRIPE_PRIVATE_KEY)
+const stripe = require("stripe")(process.env.STRIPE_PRIVATE_KEY) /*
+To get the stripe private key,create a stripe account,select test mode and access the
+private key for the test mode.
+*/
+
 
 const storeItems = new Map([
   [1, { priceInCents: 10000, name: "Learn React Today" }],
   [2, { priceInCents: 20000, name: "Learn CSS Today" }],
-])
+])  /* This is the item data,these data must be on server side 
+and not on client side.
+*/
+
 
 app.post("/create-checkout-session", async (req, res) => {
   try {
