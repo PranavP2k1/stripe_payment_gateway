0929761445fe365ab11ad72fed6cb08319fedda5
PranavP2k1 <145428124+PranavP2k1@users.noreply.github.com>
2023-09-26 16:26:57 +0530

    Update script.js

--- client/script.js
+++ client/script.js
@@ -23,3 +23,11 @@ button.addEventListener("click", () => {
       console.error(e.error)
     })
 })
+/*
+What the above code does is that it sends a POST request to the server when a button is clicked.
+The POST request contains the item details like item id and quantity.From the item id,the server
+can fetch the item price and name from the server-side database.The server then creates a URL for 
+the item checkout page with the help of stripe API.This URL is then sent to the client.The code from 
+line 19 catches the URL sent from the client and then goes to that URL using the code "window.location = url".
+*/
+
